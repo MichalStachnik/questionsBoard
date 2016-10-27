bff9854b3a9b5c201be566ff69ec5d8d4a0e5925
Michal <stachnikmichal@gmail.com>
2016-10-27 12:38:20 -0400

    querying single questions

--- api/controllers/questions.controllers.js
+++ api/controllers/questions.controllers.js
@@ -1,30 +1,57 @@
 var dbConn = require('../data/dbConnection.js');
 var questionData = require('../data/data.json');
+var ObjectId = require('mongodb').ObjectId;
 
 module.exports.questionsGetAll = function(req, res){
     
     var db = dbConn.get();
+    var collection = db.collection('questions');
     
-    console.log('db', db);
+    collection
+        .find()
+        .toArray(function(err, data){
+            console.log('found these questions', data);
     
-    console.log('GET all questions');
-    res
-        .status(200)
-        .json(questionData);
+            res
+                .status(200)
+                .json(data); 
+    });
+    
+//    console.log('db', db);
+//    
+//    console.log('GET all questions');
+//    res
+//        .status(200)
+//        .json(questionData);
 };
 
 module.exports.questionsGetOne = function(req, res){
-
+    
+    var db = dbConn.get();
+    var collection = db.collection('questions');
+    
     var questionId = req.params.qId;
-    var thisQuestion = questionData[questionId];
+    //var thisQuestion = questionData[questionId];
     console.log('GET to ' + questionId);
     
-    res
-        .status(200)
-        .json(thisQuestion);
+    collection
+        .findOne({
+            _id : ObjectId(questionId)
+    }, function(err, data){
+            res
+                .status(200)
+                .json( data );
+    });
+    
+//    res
+//        .status(200)
+//        .json(thisQuestion);
 };
 
 module.exports.questionsNew = function(req, res){
+    
+    var db = dbConn.get();
+    
     console.log("Post to new");
     console.log(req.body);
     res
